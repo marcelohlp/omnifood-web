460fbf0d3733e6161fd575ab45a5ffb27709f139
marcelo paschoal <marcelo.hlp@outlook.com>
2025-05-31 09:34:26 -0300

    feat: criando comportamento de scroll ao clicar em links da pagina

--- script/script.js
+++ script/script.js
@@ -4,7 +4,31 @@ year.textContent = currentYear
 
 const btnMobileNav = document.querySelector(".btn-mobile-nav");
 const header = document.querySelector("header");
-
 btnMobileNav.addEventListener("click", () => {
     header.classList.toggle("open-nav");
+});
+
+// SCROLLING ANIMATION 
+
+const allLinks = document.querySelectorAll('a:link');
+
+allLinks.forEach((link) => {
+    link.addEventListener("click", (e) => {
+
+        e.preventDefault();
+        const href = link.getAttribute("href");
+
+        if (href === "#") {
+            window.scroll({
+                top: 0,
+                behavior: "smooth",
+            });
+        } else if (href !== "#" && href.startsWith("#")) {
+            const section = document.querySelector(href);
+            section.scrollIntoView({behavior: "smooth"});
+        }
+
+        if (link.classList.contains("main-nav-link")) header.classList.remove("open-nav");
+
+    });
 });
